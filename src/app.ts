489ee11f038c2978b07601bb155f2f10dd756096
smlk.dev <l@smlk.dev>
2023-01-30 18:29:26 +0100

    add working example code

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import logger from "./logger";
+import * as schedule from "node-schedule";
 
 logger.info("info test message");
+
+const job = schedule.scheduleJob("*/5 * * * * *", () => {
+  logger.info("tick tock");
+});
--- src/logger.ts
+++ src/logger.ts
@@ -3,7 +3,6 @@ import * as winston from "winston";
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
   transports: [new winston.transports.File({ filename: "combined.log" })],
 });
 
